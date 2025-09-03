c7cd103387d8151422949749bae622f968c535dc
martinoalanza03 <lanzamartino3@gmail.com>
2025-09-03 11:41:20 +0200

    feat: create Console Repository

---
+++ src/main/java/com/finalproject/spring_finalproject/repository/ConsoleRepository.java
@@ -0,0 +1,9 @@
+package com.finalproject.spring_finalproject.repository;
+
+import java.io.Console;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsoleRepository extends JpaRepository<Console, Integer> {
+
+}
