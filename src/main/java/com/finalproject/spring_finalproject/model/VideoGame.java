758b3021f5df6f8903268de9d7a904fe64b7d75d
martinoalanza03 <lanzamartino3@gmail.com>
2025-09-03 11:07:14 +0200

    feat: change name table

--- src/main/java/com/finalproject/spring_finalproject/model/VideoGame.java
+++ src/main/java/com/finalproject/spring_finalproject/model/VideoGame.java
@@ -9,7 +9,7 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
-@Table(name = "videogiochi")
+@Table(name = "videogame")
 public class VideoGame {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
