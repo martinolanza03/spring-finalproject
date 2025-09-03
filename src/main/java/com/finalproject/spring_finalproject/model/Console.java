7259e6b57b1c83e1960824f07c0c26c1b2af96a4
martinoalanza03 <lanzamartino3@gmail.com>
2025-09-03 11:11:21 +0200

    feat: create table console

---
+++ src/main/java/com/finalproject/spring_finalproject/model/Console.java
@@ -0,0 +1,54 @@
+package com.finalproject.spring_finalproject.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "console")
+public class Console {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "The name must not be null, nor empty or blank")
+    private String name;
+
+    private Date releaseDate;
+
+    public Console(Integer id, String name, Date releaseDate) {
+        this.id = id;
+        this.name = name;
+        this.releaseDate = releaseDate;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getReleaseDate() {
+        return this.releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+}
