1eb1ba64ca857ef0d69b0b6155a54d8837baad3c
martinoalanza03 <lanzamartino3@gmail.com>
2025-09-05 12:36:30 +0200

    feat: add @Service

--- src/main/java/com/finalproject/spring_finalproject/security/DatabaseUserDetailsService.java
+++ src/main/java/com/finalproject/spring_finalproject/security/DatabaseUserDetailsService.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.finalproject.spring_finalproject.model.User;
 import com.finalproject.spring_finalproject.repository.UserRepository;
 
+@Service
 public class DatabaseUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
