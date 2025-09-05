50b138571a766ffe453301321c3dfd861fda4d57
martinoalanza03 <lanzamartino3@gmail.com>
2025-09-05 12:18:34 +0200

    feat: create DatabaseUserDatails

---
+++ src/main/java/com/finalproject/spring_finalproject/security/DatabaseUserDetails.java
@@ -0,0 +1,66 @@
+package com.finalproject.spring_finalproject.security;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.finalproject.spring_finalproject.model.Role;
+import com.finalproject.spring_finalproject.model.User;
+
+public class DatabaseUserDetails implements UserDetails {
+    private final Integer id;
+    private final String username;
+    private final String password;
+    private final Set<GrantedAuthority> authorities;
+
+    public DatabaseUserDetails(User user) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+
+        this.authorities = new HashSet<GrantedAuthority>();
+
+        for (Role userRole : user.getRoles()) {
+            authorities.add(new SimpleGrantedAuthority(userRole.getName()));
+        }
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public Set<GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
