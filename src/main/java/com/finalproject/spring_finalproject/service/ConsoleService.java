ef8a2d0c27fb5616bbb3ee74331da0c426016526
martinoalanza03 <lanzamartino3@gmail.com>
2025-09-05 11:44:08 +0200

    feat: add service

--- src/main/java/com/finalproject/spring_finalproject/service/ConsoleService.java
+++ src/main/java/com/finalproject/spring_finalproject/service/ConsoleService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.finalproject.spring_finalproject.model.Console;
 import com.finalproject.spring_finalproject.repository.ConsoleRepository;
 
+@Service
 public class ConsoleService {
     @Autowired
     private ConsoleRepository consoleRepository;
