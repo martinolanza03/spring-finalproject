4bef99f3219f252f1a71cadbd630e1f84f0d402d
martinoalanza03 <lanzamartino3@gmail.com>
2025-09-05 12:22:07 +0200

    feat: create DatabaseUserDetailsService

---
+++ src/main/java/com/finalproject/spring_finalproject/security/DatabaseUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.finalproject.spring_finalproject.security;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.finalproject.spring_finalproject.model.User;
+import com.finalproject.spring_finalproject.repository.UserRepository;
+
+public class DatabaseUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> userAttempt = userRepository.findByUsername(username);
+
+        if (userAttempt.isEmpty()) {
+            throw new UnsupportedOperationException("The are no users available with this username");
+        }
+
+        return new DatabaseUserDetails(userAttempt.get());
+    }
+}
