22488200c56803adaf44547c7eeaf36d5c2ccb21
martinoalanza03 <lanzamartino3@gmail.com>
2025-09-05 10:22:04 +0200

    feat: recreated the index and videogamecontroller

---
+++ src/main/java/com/finalproject/spring_finalproject/controller/VideoGameController.java
@@ -0,0 +1,37 @@
+package com.finalproject.spring_finalproject.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.finalproject.spring_finalproject.model.VideoGame;
+import com.finalproject.spring_finalproject.service.VideoGameService;
+
+@Controller
+@RequestMapping("/videogame")
+public class VideoGameController {
+    @Autowired
+    private VideoGameService videoGameService;
+
+    @GetMapping
+    public String index(Model model) {
+        List<VideoGame> videogames = videoGameService.findAll();
+
+        model.addAttribute("videogames", videogames);
+
+        return "videogame/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable Integer id, Model model) {
+        model.addAttribute("videogame", videoGameService.getById(id));
+
+        return "videogame/show";
+    }
+
+}
