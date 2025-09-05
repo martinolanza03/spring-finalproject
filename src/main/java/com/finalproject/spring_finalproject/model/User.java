c9f4e0506758d7c9d34bc31a64cdc81d5af00791
martinoalanza03 <lanzamartino3@gmail.com>
2025-09-05 12:06:18 +0200

    feat: create entity user

---
+++ src/main/java/com/finalproject/spring_finalproject/model/User.java
@@ -0,0 +1,57 @@
+package com.finalproject.spring_finalproject.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "The username must not be null, nor empty or blank")
+    private String username;
+
+    @Size(min = 6, message = "The password must have at least 6 characters")
+    @NotBlank(message = "The password must not be null, nor empty or blank")
+    private String password;
+
+    public User() {
+    }
+
+    public User(Integer id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
