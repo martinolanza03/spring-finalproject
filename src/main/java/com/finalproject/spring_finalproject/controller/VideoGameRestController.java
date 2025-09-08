86ddd52b7a8328fafb5a061b5d9d235491aad725
martinoalanza03 <lanzamartino3@gmail.com>
2025-09-08 10:38:19 +0200

    feat: create show to rest controller

--- src/main/java/com/finalproject/spring_finalproject/controller/VideoGameRestController.java
+++ src/main/java/com/finalproject/spring_finalproject/controller/VideoGameRestController.java
@@ -1,14 +1,23 @@
 package com.finalproject.spring_finalproject.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.finalproject.spring_finalproject.model.VideoGame;
 import com.finalproject.spring_finalproject.service.VideoGameService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/videogame")
@@ -23,4 +32,16 @@ public class VideoGameRestController {
 
         return videogames;
     }
+
+    @GetMapping("{id}")
+    public ResponseEntity<VideoGame> show(@Valid @PathVariable Integer id) {
+        Optional<VideoGame> videoGameAttempt = videoGameService.findById(id);
+
+        if (videoGameAttempt.isEmpty()) {
+            return new ResponseEntity<VideoGame>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<VideoGame>(videoGameAttempt.get(), HttpStatus.OK);
+    }
+
 }
