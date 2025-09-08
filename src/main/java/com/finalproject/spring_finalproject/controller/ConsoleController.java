058197b0bb82f45ece329de06e5f6cc1f76e6717
martinoalanza03 <lanzamartino3@gmail.com>
2025-09-08 12:40:16 +0200

    fix: add crosOrgin

--- src/main/java/com/finalproject/spring_finalproject/controller/ConsoleController.java
+++ src/main/java/com/finalproject/spring_finalproject/controller/ConsoleController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/console")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ConsoleController {
     @Autowired
     private ConsoleService consoleService;
