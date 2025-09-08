41afa98fd0e17dd2afe5c53f29a56c681ede6e87
martinoalanza03 <lanzamartino3@gmail.com>
2025-09-08 11:04:09 +0200

    feat: create store to ConsoleRestController

--- src/main/java/com/finalproject/spring_finalproject/controller/ConsoleRestController.java
+++ src/main/java/com/finalproject/spring_finalproject/controller/ConsoleRestController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,5 +41,10 @@ public class ConsoleRestController {
         }
 
         return new ResponseEntity<Console>(consoleAttempt.get(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Console> store(@RequestBody Console console) {
+        return new ResponseEntity<Console>(consoleService.create(console), HttpStatus.OK);
     }
 }
