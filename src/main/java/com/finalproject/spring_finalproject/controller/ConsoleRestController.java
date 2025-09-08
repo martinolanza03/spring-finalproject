0c4c7612075a74266415e84a4f4ca8b1116e8185
martinoalanza03 <lanzamartino3@gmail.com>
2025-09-08 10:55:17 +0200

    feat:  create show to ConsoleRestController

--- src/main/java/com/finalproject/spring_finalproject/controller/ConsoleRestController.java
+++ src/main/java/com/finalproject/spring_finalproject/controller/ConsoleRestController.java
@@ -1,14 +1,20 @@
 package com.finalproject.spring_finalproject.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.finalproject.spring_finalproject.model.Console;
 import com.finalproject.spring_finalproject.service.ConsoleService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/console")
@@ -22,5 +28,16 @@ public class ConsoleRestController {
         List<Console> consoles = consoleService.findAll();
 
         return consoles;
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Console> show(@Valid @PathVariable Integer id) {
+        Optional<Console> consoleAttempt = consoleService.findById(id);
+
+        if (consoleAttempt.isEmpty()) {
+            return new ResponseEntity<Console>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<Console>(consoleAttempt.get(), HttpStatus.OK);
     }
 }
